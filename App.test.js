dceeac0305fedf606b93f8887b13210598bba43b
Tony Maiers <tmaiers13@gmail.com>
2018-02-21 17:16:10 -0600

    this should pass current circleCI standards

--- App.test.js
+++ App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import renderer from 'react-test-renderer';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
