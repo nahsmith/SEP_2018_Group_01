7f93731a9b97b3baffe0abc6d5e49f565594d2e0
Tony Maiers <tmaiers13@gmail.com>
2018-02-21 17:41:17 -0600

    should pass NOW

--- App.test.js
+++ App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
+import HomeScreen from './HomeScreen'
 import renderer from 'react-test-renderer';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
@@ -7,3 +8,11 @@ it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
+
+
+/*it('home screen renders without crashing', () => {
+  const homeScreenRendered = renderer.create(<HomeScreen />).toJSON();
+  expect(homeScreenRendered).toBeTruthy();
+});*/
+
+afterAll(() => setTimeout(() => process.exit(), 1000))
