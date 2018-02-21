e7a8555bf1dd71f35fce9cac0de36d045b7c8ef6
Matthew Heim <matt@wireless-nat-inside.uiowa.edu>
2018-02-21 13:55:40 -0600

    my branch

---
+++ index.android.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import App from './src/App'
+
+const reactNavigationSample = () => {
+  return (
+    <App />
+  );
+}
+
+
+AppRegistry.registerComponent('reactNavigationSample', () => reactNavigationSample);
---
+++ index.ios.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import App from './src/App'
+import { StackNavigator } from 'react-navigation';
+import SecondScreen from './src/SecondScreen'
+
+class reactNavigationSample extends Component {
+  static navigationOptions = {
+    title: 'Home Screen',
+  };
+
+  render(){
+    const { navigation } = this.props;
+
+    return (
+      <App navigation={ navigation }/>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: reactNavigationSample },
+  SecondScreen: { screen: SecondScreen, title: 'ss' },
+});
+
+AppRegistry.registerComponent('reactNavigationSample', () => SimpleApp);
