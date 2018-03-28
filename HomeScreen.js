eccdf85e1e289455140f0c5e8a6aa706cdb556c8
Ahmed <elmaleh.ahmed@gmail.com>
2018-03-28 11:05:00 -0500

    working on populating screens

--- HomeScreen.js
+++ HomeScreen.js
@@ -3,24 +3,31 @@ import { StyleSheet, ScrollView, View, Alert, Image, Dimensions, TouchableOpacit
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 import ScrollToTop from 'react-native-scroll-to-top';
 import { StackNavigator, NavigationActions } from 'react-navigation';
-import * as Firebase from './Firebase.js';
-console.log(Firebase.test())
+import { Ionicons } from '@expo/vector-icons';
+
 const deviceWidth = Dimensions.get('window').width;
 
-Firebase.firebase.initializeApp(Firebase.config);
-var database = Firebase.firebase.database();
-var firebaseHeadingRef = Firebase.firebase.database().ref("facilities/id");
-var test1= 2;
-var stop = 0;
+//var firebase = require("firebase");
+import * as firebase from "firebase";
+//import { database } from 'firebase';
 
-
-class Gyms extends Component {
-    render() {
-      return (
-        <Text style={{textAlign: 'center', fontSize: 30}}>{this.props.name}</Text>
-      );
-    }
-  }
+// Initialize Firebase
+var src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js";
+var config = {
+ apiKey: "AIzaSyD5Ha8fSPEBkjqMchNVy8zU6vB-WvcGYTM",
+ authDomain: "herkyles-85e8e.firebaseapp.com",
+ databaseURL: "https://herkyles-85e8e.firebaseio.com",
+ projectId: "herkyles-85e8e",
+ storageBucket: "herkyles-85e8e.appspot.com",
+ messagingSenderId: "812934741247"
+};
+firebase.initializeApp(config);
+var databaseRef = firebase.database();
+var imageRef = firebase.storage();
+var test1;
+var test2;
+var arrayTest = [];
+var imageArray = [];
 
 export default class HomeScreen extends Component {
   
@@ -30,32 +37,67 @@ export default class HomeScreen extends Component {
     }
 
     componentDidMount() {
-        console.log("compDidMount HERE")
-        this.setState({})
+        
     }
 
     constructor(props) {
         super(props);
-        var tryme = "testing";
         console.log("constructor")
-        firebaseHeadingRef.on("value", function(snapshot) {
-            //return snapshot.val();
-            test1 = snapshot.val();
-            console.log(test1);
-            console.log(typeof(test1));
-            this.state={
-                gym1: test1
-            }
-            if (typeof(test1) === 'string'){
-                console.log("String!");
-            }
-            else{
-                console.log("No String!");
-            }
-            console.log("This is working: " + test1);
-        }, function (error) {
-            console.log("Error: " + error.code);
+        this.state={
+            gym1: "",
+            gym2: "",
+            image1: "",
+            image2: "",
+        }
+        var that = this;
+        var mainRef = firebase.database().ref("facilities")
+        //console.log(mainRef.once("value")); 
+        //var recCenterRef = databaseRef.ref("facilities/reccenter");
+        //var fieldHouseRef = databaseRef.ref("facilities/fieldhouse")
+        var mainImageRef = imageRef.ref("mainImages/CampusRec.jpg");
+        mainImageRef.getDownloadURL().then(function(url) {
+          console.log(url);
+          imageArray[0] = url;
+          that.setState({
+            image1 : imageArray[0],
+          })
         });
+        
+        mainRef.once("value").then(function(dataSnapshot) {
+          console.log(dataSnapshot);
+          var i = 0;
+          dataSnapshot.forEach(function(testingSnap){
+            arrayTest[i] = testingSnap.child("name").val();
+            //console.log(arrayTest[i]);
+            i++;
+          })
+          that.setState({
+            gym1 : arrayTest[0],
+            gym2 : arrayTest[1],
+          })
+        })
+
+        /*mainImageRef.getDownloadURL().then(function(url) {
+          console.log(url);
+        });
+        */
+
+        /*mainImageRef.once("value").then(function(imageSnapshot) {
+          console.log(imageSnapshot);
+          var i = 0;
+          imageSnapshot.forEach(function(testingSnap){
+            imageArray[i] = testingSnap.child.val();
+            //console.log(arrayTest[i]);
+            i++;
+          })
+          that.setState({
+            image1 : imageArray[0],
+            //gym2 : arrayTest[1],
+          })
+        })
+        */
+
+       
     }
   
   render() {
@@ -73,27 +115,17 @@ export default class HomeScreen extends Component {
 	          	 <Title>Herkyles</Title>
 	          </Body>
 	          <Right>
-              {/*
-	          <Button transparent dark onPress={() => Alert.alert('You Pressed','Back!')}>
-	          	<Icon name='arrow-dropleft' />
-	          </Button>
-              */}
 	          <Button transparent dark onPress={() => Alert.alert('You Pressed','Forums!')}>
 	          	<Icon name='chatbubbles' />
 	          </Button>
-              {/*
-	          <Button transparent dark onPress={() => this.props.navigation.dispatch(resetAction)}>
-	          	<Icon name='home' />
-	          </Button>
-              */}
 	          </Right>
 	        </Header>
 	        <Content>
 	          <ScrollView scrollsToTop={true} ref={(ref) => this.myScroll = ref}>
 	          <View style={styles.container}>
-	          <TouchableOpacity activeOpacity={ 0.75 } style={ styles.button } onPress={this.clearText}>
-            	      <Image style={{width: deviceWidth}} resizeMode='cover' source={require('./images/CampusRec.jpg')}/>
-            	      <Text style={{textAlign: 'center', fontSize: 30}}>{test1}</Text>
+	          <TouchableOpacity activeOpacity={ 0.75 } style={ styles.button } onPress={() => navigate("RecCenter", {screen: "Rec Center"})}>
+            	      <Image style={{width: deviceWidth}} resizeMode='cover' source = {{uri: this.state.image1.toString}}/>
+            	      <Text style={{textAlign: 'center', fontSize: 30}}>{'\n' + this.state.gym1}</Text>
             	      <Text style={{textAlign: 'center', fontSize: 20}}>{'\nHours'}</Text>
             	      <Text style={{textAlign: 'center', fontSize: 15}}>{'\nWeekday: 5AM - 7PM'}</Text>
             	      <Text style={{textAlign: 'center', fontSize: 15}}>{'Weekend: 5:30AM - 8PM'}</Text>
@@ -102,7 +134,7 @@ export default class HomeScreen extends Component {
 	         	<View style={styles.container}>
 	          <TouchableOpacity activeOpacity={ 0.75 } style={ styles.button } onPress={() => navigate("FieldHouse", {screen: "Field House"})}>
             	      <Image style={{width: deviceWidth}} resizeMode='cover' source={require('./images/FieldHouse.jpg')}/>
-            	      <Text style={{textAlign: 'center', fontSize: 30}}>{'\nField House'}</Text>
+            	      <Text style={{textAlign: 'center', fontSize: 30}}>{'\n' + this.state.gym2}</Text>
             	      <Text style={{textAlign: 'center', fontSize: 20}}>{'\nHours'}</Text>
             	      <Text style={{textAlign: 'center', fontSize: 15}}>{'\nWeekday: 5AM - 7PM'}</Text>
             	      <Text style={{textAlign: 'center', fontSize: 15}}>{'Weekend: 5:30AM - 8PM'}</Text>
@@ -119,18 +151,15 @@ export default class HomeScreen extends Component {
 	         	</View>
         </ScrollView>
 	        </Content>
-	        {/*
-	        <Footer>
-	          <FooterTab>
-	            <Button full>
-	              <Text>Footer</Text>
-	            </Button>
-	          </FooterTab>
-	        </Footer>
-	        */}
       </Container>
     );
   }
+}
+function refresh(test1){
+  console.log("HERE: " + test1)
+}
+function testing(test1){
+  this.setState({gym1: test1}, refresh(test1))
 }
 
 const styles = StyleSheet.create({
